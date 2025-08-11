563924124d951acc038e1be9cdb8fd1e675bac76
Divyasree Bingi <divyasree.dolly@gmail.com>
2025-08-11 18:21:48 -0500

    updated experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,18 +1,24 @@
 export const experiences = [
   {
     id: 1,
+    title: 'Full Stack Developer',
+    company: "BNY Mellon (Contract)",
+    duration: "(Mar 2025 - Present)"
+  },{
+
+    id: 2,
     title: 'Developer',
     company: "BrightMind Enrichment and Schooling",
-    duration: "(Aug 2024 - Present)"
+    duration: "(Aug 2024 - July 2025)"
   },
   {
-    id: 2,
+    id: 3,
     title: "Software Engineer",
     company: "OpenText",
     duration: "(Aug 2022 - Dec 2022)"
   },
   {
-    id: 3,
+    id: 4,
     title: "Associate Software Engineer",
     company: "OpenText",
     duration: "(Jun 2019 - July 2022)"
